9b4a29a181ceda7e49343f13c0d3c6ad5c0aa001
mrpeak <gfeng.peak@gmail.com>
2014-10-18 12:36:32 +0800

    add UI kit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/app/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
